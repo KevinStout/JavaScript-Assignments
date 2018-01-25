b01e6aa956b27f08e5c620c084905990c8f98c82
Kevin <stoutkev3@gmail.com>
2018-01-25 08:24:16 -0500

    adding in the basic structure for the html, also adding some more functions to the js

--- lesson_2/js/multipleObjects.js
+++ lesson_2/js/multipleObjects.js
@@ -11,10 +11,14 @@ function Ski(name, brand, type, length, width, price, discount){
         var totalCost = parseInt(this.price) - parseInt(this.discount);
         return "$" + totalCost;
     }
+    this.discountMessage = function(){
+        var discountReceived = "That is a " + Math.round(parseInt(this.discount)/parseInt(this.price)*100) + "% discount!";
+        return discountReceived;
+    }
 }
 
-var firstSki = new Ski('Kore 93', 'HEAD', 'All-mountain', '180cm', '93mm', '650', '10');
-var secondSki = new Ski('Soul 7', 'Rossignol', 'All-mountain', '180cm', '106mm', '850', '100');
+var firstSki = new Ski('Kore 93', 'HEAD', 'All-mountain', '180cm', '93mm', '650', '80');
+var secondSki = new Ski('Soul 7', 'Rossignol', 'All-mountain', '180cm', '106mm', '850', '150');
 
-console.log(firstSki.cost() + " " + firstSki.name + " " + firstSki.brand + " " +  firstSki.length + " " + firstSki.type + " " + firstSki.width);
-console.log("this is the second ski cost " + secondSki.cost());
+console.log(firstSki.discountMessage() + " " + firstSki.name + " " + firstSki.brand + " " +  firstSki.length + " " + firstSki.type + " " + firstSki.width);
+console.log("this is the second ski cost " + secondSki.discountMessage());
