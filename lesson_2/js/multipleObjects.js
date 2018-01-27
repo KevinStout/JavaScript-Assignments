1099591e00ab783c63a3b446782e93b2ed47a90e
Kevin <stoutkev3@gmail.com>
2018-01-27 13:45:01 -0500

    added the date function and the data to the page

--- lesson_2/js/multipleObjects.js
+++ lesson_2/js/multipleObjects.js
@@ -17,8 +17,30 @@ function Ski(name, brand, type, length, width, price, discount){
     }
 }
 
+var today = new Date();
+var weekFromToday = new Date(today.getTime() + 7*24*60*60*1000);
+
 var firstSki = new Ski('Kore 93', 'HEAD', 'All-mountain', '180cm', '93mm', '650', '80');
 var secondSki = new Ski('Soul 7', 'Rossignol', 'All-mountain', '180cm', '106mm', '850', '150');
 
-console.log(firstSki.discountMessage() + " " + firstSki.name + " " + firstSki.brand + " " +  firstSki.length + " " + firstSki.type + " " + firstSki.width);
-console.log("this is the second ski cost " + secondSki.discountMessage());
+document.getElementById("skiName1").innerHTML = firstSki.name;
+document.getElementById("skiBrand1").innerHTML = firstSki.brand;
+document.getElementById("skiType1").innerHTML = firstSki.type;
+document.getElementById("skiLength1").innerHTML = firstSki.length;
+document.getElementById("skiWidth1").innerHTML = firstSki.width;
+document.getElementById("skiPrice1").innerHTML = firstSki.price;
+document.getElementById("skiDiscount1").innerHTML = firstSki.discount;
+document.getElementById("skiCost1").innerHTML = firstSki.cost();
+document.getElementById("discountReceived1").innerHTML = firstSki.discountMessage();
+
+document.getElementById("skiName2").innerHTML = secondSki.name;
+document.getElementById("skiBrand2").innerHTML = secondSki.brand;
+document.getElementById("skiType2").innerHTML = secondSki.type;
+document.getElementById("skiLength2").innerHTML = secondSki.length;
+document.getElementById("skiWidth2").innerHTML = secondSki.width;
+document.getElementById("skiPrice2").innerHTML = secondSki.price;
+document.getElementById("skiDiscount2").innerHTML = secondSki.discount;
+document.getElementById("skiCost2").innerHTML = secondSki.cost();
+document.getElementById("discountReceived2").innerHTML = secondSki.discountMessage();
+
+document.getElementById("date").innerHTML = weekFromToday;
