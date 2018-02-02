1797f96c1d212365d0c97b40896084fc673782b5
Kevin <stoutkev3@gmail.com>
2018-02-02 09:22:02 -0500

    added a positvie number constrant to the BMI page

--- lesson_3/js/bmi.js
+++ lesson_3/js/bmi.js
@@ -2,10 +2,15 @@
 function calcBMI() {
 	var weight = eval(parseFloat(document.getElementById("weight").value));
 	var height = eval(parseFloat(document.getElementById("height").value));
-	if (weight=="" || height=="")
+	if (weight=="" || height==""){
 	    document.getElementById("msg").innerHTML="You need to enter a valid number for weight and height, try again!";
-	else if (isNaN(weight) || isNaN(height))
-    	document.getElementById("msg").innerHTML="You need to enter a valid number for weight and height, try again!";	
+	}else if (isNaN(weight) || isNaN(height)){
+		document.getElementById("msg").innerHTML="You need to enter a valid number for weight and height, try again!";
+// I did not add in any other weight and height constrants because,
+// I thought it would be fun for user to beable to put in really crazy numbers	
+	}else if (weight < 0 || height < 0){ 
+		document.getElementById("msg").innerHTML="You need to enter a valid positive number for weight and height, try again!";
+	}
 	else {   
 	var currentBMI = Math.floor((weight * 703) / (height * height));
 	document.getElementById("msg").innerHTML="Your current BMI is: " + currentBMI;
