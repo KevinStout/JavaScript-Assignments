5e9f74b826d739926d3eb3c47b51a3230d02d673
Kevin Stout <stout14@mail.nmc.edu>
2018-02-13 15:49:15 -0500

    finished windChillCalculator assignment

--- lesson_5/js/windchill.js
+++ lesson_5/js/windchill.js
@@ -1,27 +1,33 @@
-﻿function windChill() {
+﻿window.onload = getFocus();
+
+document.getElementById("reset").addEventListener("click", getFocus);
+
+document.getElementById("calc").addEventListener("click", windChill);
+
+function windChill() {
 
 	var wind=parseFloat(document.getElementById("wind").value);
 	var temp=parseFloat(document.getElementById("temp").value);
 	
-	if(isNaN(wind) || wind===0)
+	if(isNaN(wind) || wind===0){
 		document.getElementById("results").innerHTML = "<p id='error'>Please enter a number greater than zero for the wind speed!</p>";
-	else if (isNaN(temp))
+	}else if (isNaN(temp)){
 		 document.getElementById("results").innerHTML = "<p id='error'>Please enter a number for the temperature!</p>";
-	else {
+	}else {
 		var chill=(0.0817*(3.71*(Math.pow(wind, 0.5))+5.81-0.25*wind)*(temp-91.4)+91.4);
-	
-	if (chill < 0)
+	}
+	if (chill < 0){
 		document.getElementById("results").innerHTML="<p id='cold'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
-	else if (chill > 80)
+	}else if (chill > 80){
 		document.getElementById("results").innerHTML="<p id='hot'>There is no windchill factor.  Your temp is: " + Math.round(temp) + " degrees</p>";
-	else	
+	}else{	
 		document.getElementById("results").innerHTML="<p id='normal'>The windchill factor is: " + Math.round(chill) + " degrees</p>";
 	}	
-
 }
+
 function getFocus(){
 	document.getElementById("wind").focus();
-	document.getElementById("results").innerHTML=" ";
+	document.getElementById("results").innerHTML=" ";	
 }
 
 
