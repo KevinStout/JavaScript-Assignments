a072185a49c0892e3dd5e42e4947786a3ad35bd5
Kevin <stoutkev3@gmail.com>
2018-03-03 12:20:25 -0500

    built out the ajax call for the json file

---
+++ lesson_7/js/gameLibJSON.js
@@ -0,0 +1,39 @@
+$(function () {
+    function getNewData() {
+        $.ajax({
+            method: 'GET',
+            url: 'http://lisabalbach.com/stout14/CIT190/lesson_7/gameCat.json',
+            headers:{
+                accept: 'application/json'
+            },
+            dataType: 'json',
+            success: function(data){
+                //console.log(data);
+                var tr;
+                for(var i = 0; i < data.catalog.length; i++){
+                    tr = $('<tr/>');
+                    tr.append("<td>" + data.catalog[i].title + "</td>");
+                    tr.append("<td>" + data.catalog[i].publisher + "</td>");
+                    tr.append("<td>" + data.catalog[i].genre + "</td>");
+                    tr.append("<td>" + data.catalog[i].price + "</td>");
+                    tr.append("<td>" + data.catalog[i].releaseYear + "</td>");
+                    $('#mytable').append(tr)
+                }
+            },
+            statusCode:{
+                200: function(){
+                    console.log('server is working');
+                    
+                },
+                404: function(){
+                    console.log('There was a problem with the server, please try again soon.');
+                    
+                }
+            }
+        });
+    }
+
+    //change to a button
+    window.onload = getNewData;
+});
+    
