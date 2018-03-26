e3a27b72edbdcd9c20657744c66184711377e5f2
Kevin <stoutkev3@gmail.com>
2018-03-25 20:41:15 -0400

    lesson 10 exercise 2 done

---
+++ lesson_10/js/jQuaryUIAccordion.js
@@ -0,0 +1,9 @@
+$(function() {
+    $( "#accordion-menu" ).accordion({
+        collapsible:true
+    });
+     $( "#accordion-menu" ).accordion({
+         header:"ui-icon-triangle-1-e",
+         activeHeader: "ui-icon-triangle-1-s"
+     });   
+ });
